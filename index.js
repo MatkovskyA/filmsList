d8deda19f8f956bbf204e938ddea2ddb0b6b4ff4
MatkovskyA <a.matkovsky14@gmail.com>
2023-08-20 19:14:05 +0300

    Замена иконки

--- index.js
+++ index.js
@@ -1,56 +1,78 @@
-const inputMoviesNode = document.getElementById('input-text'); //данные инпута
-const movieBtnAdd = document.getElementById('button-arrow'); // кнопка
-const moviesListNode = document.getElementById('movies__lists'); // список выводимых данных
+// форма
+const form = document.querySelector("#form")
+//данные инпута
+const inputMovies = document.getElementById('input'); 
+// кнопка добавления значения из инпута
+const movieAddBtn = document.getElementById('button__add'); 
+// список выводимых данных
+const moviesList = document.getElementById('movies__list_item'); 
+
+let movieArr = []
 
 
-//получаем данные из input + проверка
-function addMovie() {
-    if (!inputMoviesNode.value.trim()) {
-        alert("Введите корректное название фильма");
+//проверяем значение input
+// получаем и передаем название фильма в список
+function getDataMovie() {
+    let movieHTML = `
+    <li id="movies__list_empty" class="movies__list_empty">
+        <button type="button" data-action ="checked" class="movie__btn_checked"></button>
+        <span data-action ="mark" class="movie__title">${inputMovies.value}</span>
+        <button type="button" data-action ="delete" class="movie__btn_delete cross"></button>
+    </li>
+    `;
+    return movieHTML
+
+}
+
+// добавляем фильм в конец списока
+function renderList() {
+    moviesList.insertAdjacentHTML('beforeend', getDataMovie() )
+}
+
+// отображаем список фильмов 
+function getMovie(e) {
+    // отмена отправки формы
+    e.preventDefault();
+
+    renderList();
+    //обнуляем поле ввода
+    inputMovies.value = "";
+    //оставляем на нем фокус
+    inputMovies.focus();
+}
+
+// удаление фильма
+function deleteMovie(e) {
+    if (e.target.dataset.action === 'delete') {
+        const parentNode = e.target.closest('li');
+        parentNode.remove()
     }
-    if (inputMoviesNode.value.trim()) {
-        createMovieList();
-    }
-    if (inputMoviesNode.value.length > '100') {
-        alert('Слишком большое название фильма, ты не ошибся?');
+}
+
+// зачеркивание фильма
+function markMovie(e) {
+    // проверяем, что клик пришелся по названию фильма
+    if (e.target.dataset.action === 'checked') {
+        const parentNode = e.target.closest('li');
+
+        const movieBtnChecked = parentNode.querySelector('.movie__btn_checked')
         
+        movieBtnChecked.classList.toggle('movie__btn_check')
+        const movieTitle = parentNode.querySelector('.movie__title')
+        movieTitle.classList.toggle('movie__title_mark');
+        parentNode.classList.toggle('li_bg_color')
     }
-    inputMoviesNode.value = '';
-    saveData();
+    if (e.target.dataset.action === "mark") {
+        const parentNode = e.target.closest('li');
+        const movieTitle = parentNode.querySelector('.movie__title')
+        movieTitle.classList.toggle('movie__title_mark');
+    }
 }
 
-// создаем список с иконкой закрытия
-function createMovieList() {
-    let movieList = document.createElement('li');
-    movieList.innerHTML = inputMoviesNode.value;
-    moviesListNode.appendChild(movieList);
-    let cross__delete = document.createElement('span');
-    cross__delete.innerHTML = '+';
-    movieList.appendChild(cross__delete);
-}
 
-// 
-function checkedMovie(el) {
-    if (el.target.tagName === 'LI') {
-        el.target.classList.toggle('checked');
-        saveData()
-    }
-    if (el.target.tagName === "SPAN") {
-        el.target.parentElement.remove()
-        saveData()
-    }
-    return
-}
-// сохранение в  LS
-function saveData() {
-    localStorage.setItem('data', moviesListNode.innerHTML);
-}
-// Отображение сохраненных данных
-showMovie()
-function showMovie() {
-    moviesListNode.innerHTML = localStorage.getItem('data')
-}
-// Зачеркивание / удаление фильма через клик
-moviesListNode.addEventListener('click', checkedMovie)
-// добаление в список фильма 
-movieBtnAdd.addEventListener('click', addMovie)
+// обработчик для вывода фильма
+form.addEventListener('submit', getMovie);
+//обработчик для удаления фильма
+moviesList.addEventListener('click', deleteMovie)
+// обработчик для зачеркивания фильма
+moviesList.addEventListener('click', markMovie)
